053fa447e23a89be306ce989c9b83e080e0d484b
Ian Morton <ian.morton@sky.uk>
2017-04-18 14:20:14 +0100

    add cg class

---
+++ cg.js
@@ -0,0 +1,108 @@
+var net = require('net');
+
+var CG = {
+
+    ip: '1.2.3.4',
+    port: 8080,
+    replies: true,
+    quiet: false,
+    live: true,
+    counter: 0,
+    logging: 1,
+    socket: 0,
+
+    open: function() {
+        dlog('Opening a port to CG...' + this.ip + ", " + this.port);
+
+        var _this = this;
+
+        this.socket = net.connect({host: this.ip, port: this.port},
+            function() { //'connect' listener
+                _this.live = true;
+                dlog('client open');
+            });
+
+        this.socket.on('data', function(data) {
+            dlog('received: ' + data);
+        });
+
+        this.socket.on('end', function() {
+            this.live = false;
+            dlog('client ended');
+        });
+
+        this.socket.on('close', function() {
+            this.live = false;
+            dlog('client closed');
+        });
+
+        this.socket.on('timeout', function() {
+            dlog('client timed out');
+        });
+
+        this.socket.on('error', function() {
+            dlog('client error');
+        });
+
+        this.socket.on('connect', function() {
+            dlog('client connected');
+        });
+
+    },
+
+    version: function() {
+        this.sendCommand("MAIN VERSION");
+    },
+
+    replySwap: function() {
+        this.replies = !this.replies;
+    },
+
+    quietSwap: function() {
+        this.quiet = !this.quiet;
+    },
+
+    sendCommand: function(cmd) {
+
+        //   dlog('Live?: ' + this.live);
+
+        if (!this.live) {
+            dlog('Re-opening a port to CG...' + this.ip + ", " + this.port);
+            this.open();
+        }
+
+        if (this.replies  && !this.quiet) {
+            this.counter++;
+            this.socket.write(this.counter + " " + cmd + "\0");
+        } else {
+            if (this.quiet) {
+                this.socket.write("@ " + cmd + "\0");
+            } else  {
+                this.socket.write("-1 " + cmd + "\0");
+            }
+        }
+    },
+
+    end: function() {
+        this.live = false;
+        this.socket.end();
+    },
+
+    close: function() {
+        this.live = false;
+        this.socket.end();
+    },
+
+    destroy: function() {
+        this.live = false;
+        this.socket.destroy();
+    }
+};
+
+function dlog(data) {
+    if (CG.logging > 0) {
+        console.log('\x1b[36m\>>>\x1b[37m ' + data);
+    }
+}
+
+module.exports = CG;
--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+var readline = require('readline');
+var rl;
 var net = require('net');
 var port = 8080;
 
@@ -28,6 +30,51 @@ function dlog(data) {
         console.log('\x1b[36m\>>>\x1b[37m ' + data);
 }
 client.connect(port, '127.0.0.1', function() {
-	console.log('Connected');
+	console.log('\x1b[36m Connected to port: ' + port + '\x1b[37m');
 	client.write('Hello world.');
 });
+
+rl = readline.createInterface(process.stdin, process.stdout, null);
+rl.setPrompt('> ');
+
+console.log('\nWaiting for commands...("quit" to exit)');
+
+rl.on('line', function(cmd) {
+
+    if (cmd === 'quit') {
+        rl.question('Are you sure? (y/n) ', function(answer) {
+            if (answer === 'y') {
+                rl.close();
+            } else {
+                rl.prompt();
+            }
+        });
+    } else {
+        // parse the command
+        //
+        if (cmd === "v") {
+            viz.version(false);
+        } else if (cmd === "r") {
+            viz.replySwap();
+        } else if (cmd === "q") {
+            viz.quietSwap();
+        } else if (cmd === "d") {
+            viz.destroy();
+        } else if (cmd === "e") {
+            viz.end();
+        } else {
+            viz.sendCommand(cmd);       // eg MAIN VERSION
+        }
+        //    console.log('You typed:', cmd);
+        //    console.log('Type "quit" to exit');
+        rl.prompt();
+    }
+
+});
+
+rl.on('close', function() {
+    console.log('Bye');
+    process.exit();
+});
+
+rl.prompt();
